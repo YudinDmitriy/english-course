8950ffa566f12ccf57c815131588ca68d601991f
Yudin Dmitriy <yudin01042002@yandex.ru>
2025-02-22 01:51:02 +0300

    learning_content, them_plan

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from main.views import index
+from main.views import learning_content, educational_and_thematic_plan, glossary
 
 app_name = 'main'
 
 
 urlpatterns = [
-    path('', index),
+    path('', learning_content, name='start_list'),
+    path('/thematic_plan', educational_and_thematic_plan, name='tem_plan'),
+    path('/glossary', glossary, name='glossary'),
 
 ]
 
--- main/views.py
+++ main/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'main/base2.html')
+def learning_content(request):
+    return render(request, 'main/learning_content.html')
+
+
+def educational_and_thematic_plan(request):
+    return render(request, 'main/education_and_thematic_plan.html')
+
+def glossary(request):
+    return  render(request,'main/glossary.html')
+
